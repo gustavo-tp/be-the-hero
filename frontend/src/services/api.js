7283d9e4edc2f05df813b622522140610f041894
gustavo-tp <gustavo1190.gt@gmail.com>
2020-04-09 03:14:33 -0300

    updated api url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://dev-radar-gustavo-tp.herokuapp.com',
+  baseURL: 'https://be-the-hero-gustavo-tp.herokuapp.com',
 });
 
 export default api;
